aac2f7f6b2373582def87d552a3e958368233316
dzianis <kisluka53773@gmail.com>
2024-06-10 12:31:43 +0200

    fourth task finished

--- templates.php
+++ templates.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 function findRepeatingWord(string $str, int $amount): array
 {
-    $pattern = "/\\b(\\w+)\\s+\\1{{$amount}}\\b/i";
+    $pattern = '/\b((\w+)\s+){' . $amount . '}\b/i';
 
     echo $pattern;
 
     preg_match_all($pattern, $str, $matches);
 
-    return $matches ?? [];
+    return $matches[0] ?? [];
 }
 
 $string = 'word word hello world data data data';
 
-var_dump(findRepeatingWord($string, 0));
+var_dump(findRepeatingWord($string, 4));
+
+function generateHtmlLinks(array $urls): array
+{
+    $links = [];
+
+    foreach ($urls as $url) {
+        $links [] = '<a href="' . $url . '">Visit ' . $url .'</a>';
+    }
+
+    return $links;
+}
+
+$links = ['https://www.w3schools.com', 'https://google.com'];
+
+var_dump(generateHtmlLinks($links));
